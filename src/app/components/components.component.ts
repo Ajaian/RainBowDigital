021915572abda0ac19fd37b2687397a8f1810645
ajaian <ajaianc.a1011@gmail.com>
2025-08-10 11:00:18 +0530

    Save work before Angular update

--- src/app/components/components.component.ts
+++ src/app/components/components.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { productDetails } from '../services/details.constants';
+import { CommonModule } from '@angular/common';
+import { ProductDetailsComponent } from '../shared/product-details/product-details.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-components',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './components.component.html',
   styleUrl: './components.component.css'
 })
-export class ComponentsComponent {
+export class ComponentsComponent implements OnInit {
+  productdetails: any[] = [];
+  constructor(private http: HttpClient,  private modalService: NgbModal) {  }
 
+  ngOnInit() {
+    debugger;
+    this.productdetails = productDetails;
+  }
+  onProductClick(product: any) {
+    debugger;
+    const modalReference = this.modalService.open(ProductDetailsComponent, { size: 'lg' });
+    modalReference.componentInstance.product = product; // Pass the product data to the modal
+  }
 }
---
+++ src/app/services/details.constants.ts
@@ -0,0 +1,34 @@
+export const productDetails = [
+      {
+        "imageURL":"picture_frame-1.jpg",
+        "productName":"Product 1"
+      }, 
+      {
+        "imageURL":"picture_frame-2.jpg",
+        "productName":"Product 2"
+      },
+      {
+        "imageURL":"picture_frame-3.jpg",
+        "productName":"Product 3"
+      },
+      {
+        "imageURL":"picture_frame-4.jpg",
+        "productName":"Product 4"
+      },
+      {
+        "imageURL":"picture_frame-5.jpg",
+        "productName":"Product 5"
+      },
+      {
+        "imageURL":"picture_frame-6.jpg",
+        "productName":"Product 6"
+      },
+      {
+        "imageURL":"picture_frame-7.jpg",
+        "productName":"Product 7"
+      },
+      {
+        "imageURL":"picture_frame-8.jpg",
+        "productName":"Product 8"
+      }
+    ];
